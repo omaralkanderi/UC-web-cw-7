13d3bbf42b5147497f71b433854d631436c1cdca
omaralkanderi <112582096+omaralkanderi@users.noreply.github.com>
2022-09-21 20:10:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,16 +7,27 @@ let random_grade_div = document.getElementById("random-grade");
 
 
 /*  الجزء الأول والثاني من التمرين */
+grades.pop()
+grades.push("99")
+// let i = 0;
+// let len = grades.length;
+// for (; i < len; ) {
 
+  
+// }
+// document.getElementById("item").innerHTML =i;
+// grades.forEach(all_grades_div.innerHTML += `<span>${item}<span>`);    
 
-
-
-
-
-
-
+grades.forEach(item => {all_grades_div.innerHTML += `<span>${item}<span>`});
 
 /* الجزء الثالث من التمرين */
+
+// let randomIndex =grades.length *Math.random()
+let randomIndex = Math.floor(Math.random() * grades.length)
+//  let any=grades[randomIndex];
+
+ random_grade_div.innerHTML = `<span>#${randomIndex}: ${grades[randomIndex]}<span>`
+  
 
 let course = {
   name: "UniCODE",
@@ -25,13 +36,43 @@ let course = {
   location : "Kuwait University"
 }
 
+
 //Change studentsCount,track property here
 
-
+course.track="web"
+course.studentsCount=40;
 
 //Console log here
+console.log("Hello my name is"+course.name +"im in "+ course.track +"at" + course.studentsCount)
 
 
 
 //Create student object here
+let student ={
+  name: "omar",
+  university:"kuniv"
+}
+
+
+let course1 = [ 
+{
+  name: "UniCODE",
+  track : "one",
+  location : "Kuwait University"
+},
+{
+  name: "UniCODE",
+  track : "two",
+  location : "Kuwait University"
+},
+{
+  name: "UniCODE",
+  track : "three",
+  location : "Kuwait University"
+}
+]
+let UniCODEs=course1
+UniCODEs.forEach(track => {console.log(track)
+  
+});
 
